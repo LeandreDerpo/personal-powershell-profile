01077f08a9f611a972858e6016e3ebcf40e2e66d
Jlder <derpojoseph@gmail.com>
2023-01-03 21:48:05 +0700

    First commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,74 @@
+### PowerShell template profile 
+### Version 1.00 - Joseph Leandre Derpo <derpojoseph@gmail.com>
+###
+### This file should be stored in $PROFILE.CurrentUserAllHosts
+### If $PROFILE.CurrentUserAllHosts doesn't exist, you can make one with the following:
+###    PS> New-Item $PROFILE.CurrentUserAllHosts -ItemType File -Force
+### This will create the file and the containing subdirectory if it doesn't already 
+###
+### As a reminder, to enable unsigned script execution of local scripts on client Windows, 
+### you need to run this line (or similar) from an elevated PowerShell prompt:
+###   Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+### This is the default policy on Windows Server 2012 R2 and above for server Windows. For 
+### more information about execution policies, run Get-Help about_Execution_Policies.
+
+### Inspiration taken from ChrisTitusTech and devaslife in setting up this PowerShell prompt
+
+# Prompt
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\uew.omp.json" | Invoke-Expression
+
+# Icons
+Import-Module -Name Terminal-Icons 
+
+# PSReadLine
+Import-Module -Name PSReadLine
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History 
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# z
+Import-Module z
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+# Set-Alias grep findstr
+# Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+# Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function find-file($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
+}
+
+# Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
+function dirs {
+    if ($args.Count -gt 0) {
+        Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
+    } else {
+        Get-ChildItem -Recurse | Foreach-Object FullName
+    }
+}
+
+# Stops a specified process from running
+function pkill($name) {
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
+
+function pgrep($name) {
+    Get-Process $name
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Personal Powershell Profile
+Make powershell profile look good with themes and neat shortcuts.
+
+## Themes
+JanDeDobbeleer/oh-my-posh prompt theme engine was used to make the customization of the powershell happen.
+
+## Fix Missing Fonts
+Make sure to install nerd fonts for missing fonts to be fixed, in my case, Hack Bold Nerd Font was installed.
